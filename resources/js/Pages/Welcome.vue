1207ecc2bfb647596011a18169cac17bf23f89d1
Yom <yomssouza@gmail.com>
2022-01-16 10:02:51 -0300

    trust proxies

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <Head title="Welcome" />
+  <Head title="Bem-vindo" />
 
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
     <div
