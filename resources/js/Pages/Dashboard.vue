5735d0b2c5b496b8c496c295d03b62694578bd32
Yom <yomssouza@gmail.com>
2022-01-15 22:45:40 -0300

    fix dashboard initialization

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -417,25 +417,6 @@
         </Dialog>
       </TransitionRoot>
     </template>
-    <!-- <button
-              class="bg-purple-300"
-              title="Favoritar"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 fill-current hover:text-amber-400 rounded-full"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
-            </button> -->
   </BreezeAuthenticatedLayout>
 </template>
 
@@ -510,7 +491,7 @@ export default {
     return {
       devSelected: {},
       devs: {},
-      isOpen: true,
+      isOpen: false,
       languages: [
         { text: 'PHP', val: 'php' },
         { text: 'JS', val: 'javascript' },
@@ -535,10 +516,7 @@ export default {
       if (reset) this.search.page = 1
 
       devsService.get(this.search)
-        .then(data => {
-          this.devs = data
-          this.devDetails(this.devs.items[0])
-        })
+        .then(data => this.devs = data)
     },
     next () {
       if (this.devs.total_count === 0 || (this.devs.total_count % this.search.per_page) > this.search.page) return
@@ -569,7 +547,6 @@ export default {
   },
   mounted () {
     this.resetSearch()
-    this.getDevs()
   }
 }
 </script>
